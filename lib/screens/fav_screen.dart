296c7301a3f0dc20e164fac669b308ba58d5c158
Sherif Nafee <shico.m.nafee@gmail.com>
2022-12-19 17:07:42 +0300

    Clean up fav screen

--- lib/screens/fav_screen.dart
+++ lib/screens/fav_screen.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:soar_quest/soar_quest.dart';
 
-class FavoriteJokes {
-  static late final SQCollection favJokesCollection;
-  static getFavoriteScreen(userID) {
-    favJokesCollection = FirestoreCollection(
-        id: "Favourites",
-        fields: [SQStringField("Joke")],
-        updates: false,
-        adds: false,
-        parentDoc: SQDoc(userID, collection: SQAuth.usersCollection));
+import '../device_id.dart';
 
-    return CollectionScreen(
-        collection: favJokesCollection, icon: Icons.favorite);
-  }
+late SQCollection favJokesCollection;
 
-  static getFavJokes() {
-    return favJokesCollection;
-  }
+Future<void> initFavsCollection() async {
+  favJokesCollection = FirestoreCollection(
+      id: "Favourites",
+      fields: [SQStringField("Joke")],
+      updates: false,
+      adds: false,
+      parentDoc:
+          SQDoc(DeviceIdentifier.deviceId, collection: SQAuth.usersCollection));
+}
+
+class FavoriteJokesScreen extends CollectionScreen {
+  FavoriteJokesScreen({required super.collection, super.key})
+      : super(icon: Icons.favorite);
 }
