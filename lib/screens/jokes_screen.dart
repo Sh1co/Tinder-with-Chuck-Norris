63aead7e38aad6e7ee7882706447b34576408271
Sherif Nafee <shico.m.nafee@gmail.com>
2022-12-19 17:07:32 +0300

    Clean up jokes screen

--- lib/screens/jokes_screen.dart
+++ lib/screens/jokes_screen.dart
@@ -24,7 +24,7 @@ class JokesScreenState extends ScreenState<JokesScreen> {
 
           final SwipeItem? currentItem = _matchEngine.currentItem;
           if (currentItem == null) return;
-          var favJokesCollection = FavoriteJokes.getFavJokes();
+
           favJokesCollection.saveDoc(favJokesCollection.newDoc(initialFields: [
             SQStringField("Joke", value: currentItem.content)
           ]));
@@ -65,49 +65,78 @@ class JokesScreenState extends ScreenState<JokesScreen> {
         child: SwipeCards(
           matchEngine: _matchEngine,
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-                alignment: Alignment.center,
-                color: Colors.brown,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Image(
-                          image: AssetImage('graphics/chuck-norris.png'),
-                          width: 200,
-                        ),
-                      ),
-                      Text(
-                        _swipeItems[index].content,
-                        style:
-                            const TextStyle(fontSize: 15, color: Colors.white),
-                        textAlign: TextAlign.center,
-                      )
-                    ],
-                  ),
-                ));
+            return CNSwipeCard(swipeItems: _swipeItems, cardIndex: index);
           },
           onStackFinished: () {},
           upSwipeAllowed: false,
           fillSpace: true,
         ),
       ),
-      Padding(
-        padding: const EdgeInsets.fromLTRB(0, 13, 0, 13),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SQButton.icon(Icons.thumb_down,
-                onPressed: () => _matchEngine.currentItem?.nope(),
-                iconSize: 50),
-            SQButton.icon(Icons.thumb_up,
-                onPressed: () => _matchEngine.currentItem?.like(),
-                iconSize: 50),
-          ],
-        ),
-      ),
+      VoteButtons(matchEngine: _matchEngine),
     ]);
+  }
+}
+
+class CNSwipeCard extends StatelessWidget {
+  const CNSwipeCard({
+    Key? key,
+    required List<SwipeItem> swipeItems,
+    required this.cardIndex,
+  })  : _swipeItems = swipeItems,
+        super(key: key);
+
+  final List<SwipeItem> _swipeItems;
+  final int cardIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        color: Colors.brown,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Image(
+                  image: AssetImage('graphics/chuck-norris.png'),
+                  width: 200,
+                ),
+              ),
+              Text(
+                _swipeItems[cardIndex].content,
+                style: const TextStyle(fontSize: 15, color: Colors.white),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class VoteButtons extends StatelessWidget {
+  const VoteButtons({
+    Key? key,
+    required MatchEngine matchEngine,
+  })  : _matchEngine = matchEngine,
+        super(key: key);
+
+  final MatchEngine _matchEngine;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 13, 0, 13),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SQButton.icon(Icons.thumb_down,
+              onPressed: () => _matchEngine.currentItem?.nope(), iconSize: 50),
+          SQButton.icon(Icons.thumb_up,
+              onPressed: () => _matchEngine.currentItem?.like(), iconSize: 50),
+        ],
+      ),
+    );
   }
 }
